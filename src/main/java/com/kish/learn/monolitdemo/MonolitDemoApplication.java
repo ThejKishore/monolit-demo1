3ec518d3da6157c2ac9b9890111cbb1eaf7a3260
Abirami <abiramibcse@gmail.com>
2021-11-09 00:56:58 -0600

    First commit with react front end integrated

---
+++ src/main/java/com/kish/learn/monolitdemo/MonolitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kish.learn.monolitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonolitDemoApplication.class, args);
+    }
+
+}
